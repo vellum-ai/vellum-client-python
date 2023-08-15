cc8a42630626ba45b140df2faa1df1ff31feb784
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2023-08-15 18:39:50 +0000

    Release 0.0.33

--- src/vellum/types/workflow_execution_event_error_code.py
+++ src/vellum/types/workflow_execution_event_error_code.py
@@ -9,23 +9,28 @@ T_Result = typing.TypeVar("T_Result")
 class WorkflowExecutionEventErrorCode(str, enum.Enum):
     """
     * `WORKFLOW_INITIALIZATION` - WORKFLOW_INITIALIZATION
-    * `NODE_EXECUTION` - NODE_EXECUTION
     * `NODE_EXECUTION_COUNT_LIMIT_REACHED` - NODE_EXECUTION_COUNT_LIMIT_REACHED
+    * `NODE_EXECUTION` - NODE_EXECUTION
+    * `LLM_PROVIDER` - LLM_PROVIDER
     """
 
     WORKFLOW_INITIALIZATION = "WORKFLOW_INITIALIZATION"
-    NODE_EXECUTION = "NODE_EXECUTION"
     NODE_EXECUTION_COUNT_LIMIT_REACHED = "NODE_EXECUTION_COUNT_LIMIT_REACHED"
+    NODE_EXECUTION = "NODE_EXECUTION"
+    LLM_PROVIDER = "LLM_PROVIDER"
 
     def visit(
         self,
         workflow_initialization: typing.Callable[[], T_Result],
-        node_execution: typing.Callable[[], T_Result],
         node_execution_count_limit_reached: typing.Callable[[], T_Result],
+        node_execution: typing.Callable[[], T_Result],
+        llm_provider: typing.Callable[[], T_Result],
     ) -> T_Result:
         if self is WorkflowExecutionEventErrorCode.WORKFLOW_INITIALIZATION:
             return workflow_initialization()
-        if self is WorkflowExecutionEventErrorCode.NODE_EXECUTION:
-            return node_execution()
         if self is WorkflowExecutionEventErrorCode.NODE_EXECUTION_COUNT_LIMIT_REACHED:
             return node_execution_count_limit_reached()
+        if self is WorkflowExecutionEventErrorCode.NODE_EXECUTION:
+            return node_execution()
+        if self is WorkflowExecutionEventErrorCode.LLM_PROVIDER:
+            return llm_provider()

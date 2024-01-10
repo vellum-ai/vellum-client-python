d7674c047030f8ff0ccfca3e65b0a3a729dc62cb
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-01-10 19:58:25 +0000

    Release v0.1.13

--- src/vellum/core/client_wrapper.py
+++ src/vellum/core/client_wrapper.py
@@ -16,7 +16,7 @@ class BaseClientWrapper:
         headers: typing.Dict[str, str] = {
             "X-Fern-Language": "Python",
             "X-Fern-SDK-Name": "vellum-ai",
-            "X-Fern-SDK-Version": "0.1.12",
+            "X-Fern-SDK-Version": "v0.1.13",
         }
         headers["X_API_KEY"] = self.api_key
         return headers

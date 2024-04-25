53869da47aa07af9a4871a405e37ef95b90b80d0
Noa Flaherty <noa@vellum.ai>
2024-04-25 11:39:21 -0400

    Run black

--- src/vellum/lib/utils/env.py
+++ src/vellum/lib/utils/env.py
@@ -6,6 +6,8 @@ from .exceptions import VellumClientException
 def get_api_key() -> str:
     api_key = os.environ.get("VELLUM_API_KEY")
     if api_key is None:
-        raise VellumClientException("`VELLUM_API_KEY` environment variable id required to be set.")
-    
+        raise VellumClientException(
+            "`VELLUM_API_KEY` environment variable id required to be set."
+        )
+
     return api_key

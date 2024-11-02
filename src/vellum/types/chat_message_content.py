7b64e75dd29f23927923184ce1c7e1927acbf966
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-11-02 11:35:12 +0000

    SDK regeneration

--- src/vellum/types/chat_message_content.py
+++ src/vellum/types/chat_message_content.py
@@ -5,7 +5,12 @@ from .string_chat_message_content import StringChatMessageContent
 from .function_call_chat_message_content import FunctionCallChatMessageContent
 from .array_chat_message_content import ArrayChatMessageContent
 from .image_chat_message_content import ImageChatMessageContent
+from .audio_chat_message_content import AudioChatMessageContent
 
 ChatMessageContent = typing.Union[
-    StringChatMessageContent, FunctionCallChatMessageContent, ArrayChatMessageContent, ImageChatMessageContent
+    StringChatMessageContent,
+    FunctionCallChatMessageContent,
+    ArrayChatMessageContent,
+    ImageChatMessageContent,
+    AudioChatMessageContent,
 ]

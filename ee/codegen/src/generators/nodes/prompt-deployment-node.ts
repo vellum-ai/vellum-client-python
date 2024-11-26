addf1ac29842e442051ac5f5de9a3d75ec20e0a5
Noa Flaherty <noa@vellum.ai>
2024-11-25 22:36:20 -0500

    Introduce node

---
+++ ee/codegen/src/generators/nodes/prompt-deployment-node.ts
@@ -0,0 +1,163 @@
+import { python } from "@fern-api/python-ast";
+import { AstNode } from "@fern-api/python-ast/core/AstNode";
+
+import { OUTPUTS_CLASS_NAME } from "src/constants";
+import { PromptDeploymentNodeContext } from "src/context/node-context/prompt-deployment-node";
+import { BaseSingleFileNode } from "src/generators/nodes/bases/single-file-base";
+import { DeploymentPromptNodeData, PromptNode } from "src/types/vellum";
+
+export class PromptDeploymentNode extends BaseSingleFileNode<
+  PromptNode,
+  PromptDeploymentNodeContext
+> {
+  baseNodeClassName = "PromptDeploymentNode";
+  baseNodeDisplayClassName = "BasePromptDeploymentNodeDisplay";
+
+  protected getNodeClassBodyStatements(): AstNode[] {
+    const statements: AstNode[] = [];
+
+    if (this.nodeData.data.variant !== "DEPLOYMENT") {
+      throw new Error(
+        `PromptDeploymentNode only supports DEPLOYMENT variant. Received ${this.nodeData.data.variant}`
+      );
+    }
+
+    const nodeData: DeploymentPromptNodeData = this.nodeData.data;
+
+    statements.push(
+      python.field({
+        name: "deployment",
+        initializer: python.TypeInstantiation.str(nodeData.deploymentId),
+      })
+    );
+
+    statements.push(
+      python.field({
+        name: "release_tag",
+        initializer: python.TypeInstantiation.str(nodeData.releaseTag),
+      })
+    );
+
+    statements.push(
+      python.field({
+        name: "prompt_inputs",
+        initializer: python.TypeInstantiation.dict(
+          Array.from(this.nodeInputsByKey.entries()).map(([key, value]) => ({
+            key: python.TypeInstantiation.str(key),
+            value: value,
+          }))
+        ),
+      })
+    );
+
+    return statements;
+  }
+
+  protected getNodeDisplayClassBodyStatements(): AstNode[] {
+    const statements: AstNode[] = [];
+
+    statements.push(
+      python.field({
+        name: "label",
+        initializer: python.TypeInstantiation.str(this.nodeData.data.label),
+      }),
+      python.field({
+        name: "node_id",
+        initializer: python.TypeInstantiation.uuid(this.nodeData.id),
+      }),
+      python.field({
+        name: "output_id",
+        initializer: python.TypeInstantiation.uuid(this.nodeData.data.outputId),
+      }),
+      python.field({
+        name: "array_output_id",
+        initializer: python.TypeInstantiation.uuid(
+          this.nodeData.data.arrayOutputId
+        ),
+      }),
+      python.field({
+        name: "target_handle_id",
+        initializer: python.TypeInstantiation.uuid(
+          this.nodeData.data.targetHandleId
+        ),
+      }),
+      python.field({
+        name: "prompt_input_ids_by_name",
+        initializer: python.TypeInstantiation.dict(
+          this.nodeData.inputs.map((input) => ({
+            key: python.TypeInstantiation.str(input.key),
+            value: python.TypeInstantiation.uuid(input.id),
+          }))
+        ),
+      })
+    );
+
+    return statements;
+  }
+
+  protected getOutputDisplay(): python.Field {
+    return python.field({
+      name: "output_display",
+      initializer: python.TypeInstantiation.dict([
+        {
+          key: python.reference({
+            name: this.nodeContext.nodeClassName,
+            modulePath: this.nodeContext.nodeModulePath,
+            attribute: [OUTPUTS_CLASS_NAME, "text"],
+          }),
+          value: python.instantiateClass({
+            classReference: python.reference({
+              name: "NodeOutputDisplay",
+              modulePath:
+                this.workflowContext.sdkModulePathNames
+                  .NODE_DISPLAY_TYPES_MODULE_PATH,
+            }),
+            arguments_: [
+              python.methodArgument({
+                name: "id",
+                value: python.TypeInstantiation.uuid(
+                  this.nodeData.data.outputId
+                ),
+              }),
+              python.methodArgument({
+                name: "name",
+                value: python.TypeInstantiation.str("text"),
+              }),
+            ],
+          }),
+        },
+        {
+          key: python.reference({
+            name: this.nodeContext.nodeClassName,
+            modulePath: this.nodeContext.nodeModulePath,
+            attribute: [OUTPUTS_CLASS_NAME, "results"],
+          }),
+          value: python.instantiateClass({
+            classReference: python.reference({
+              name: "NodeOutputDisplay",
+              modulePath:
+                this.workflowContext.sdkModulePathNames
+                  .NODE_DISPLAY_TYPES_MODULE_PATH,
+            }),
+            arguments_: [
+              python.methodArgument({
+                name: "id",
+                value: python.TypeInstantiation.uuid(
+                  this.nodeData.data.arrayOutputId
+                ),
+              }),
+              python.methodArgument({
+                name: "name",
+                value: python.TypeInstantiation.str("results"),
+              }),
+            ],
+          }),
+        },
+      ]),
+    });
+  }
+
+  protected getErrorOutputId(): string | undefined {
+    return this.nodeData.data.errorOutputId;
+  }
+}

8c34a97d1a293f6e451266b20f03b009f2dcbc43
Noa Flaherty <noa@vellum.ai>
2024-11-25 20:16:13 -0500

    Add test

---
+++ ee/codegen/src/__test__/nodes/final-output-node.test.ts
@@ -0,0 +1,46 @@
+import { Writer } from "@fern-api/python-ast/core/Writer";
+import { beforeEach } from "vitest";
+
+import { workflowContextFactory } from "src/__test__/helpers";
+import { finalOutputNodeFactory } from "src/__test__/helpers/node-data-factories";
+import { createNodeContext, WorkflowContext } from "src/context";
+import { FinalOutputNodeContext } from "src/context/node-context/final-output-node";
+import { FinalOutputNode } from "src/generators/nodes/final-output-node";
+
+describe("FinalOutputNode", () => {
+  let workflowContext: WorkflowContext;
+  let writer: Writer;
+  let node: FinalOutputNode;
+
+  beforeEach(() => {
+    workflowContext = workflowContextFactory();
+    writer = new Writer();
+  });
+
+  describe("basic", () => {
+    beforeEach(() => {
+      const nodeData = finalOutputNodeFactory();
+
+      const nodeContext = createNodeContext({
+        workflowContext,
+        nodeData,
+      }) as FinalOutputNodeContext;
+      workflowContext.addNodeContext(nodeContext);
+
+      node = new FinalOutputNode({
+        workflowContext: workflowContext,
+        nodeContext,
+      });
+    });
+
+    it("getNodeFile", async () => {
+      node.getNodeFile().write(writer);
+      expect(await writer.toStringFormatted()).toMatchSnapshot();
+    });
+
+    it("getNodeDisplayFile", async () => {
+      node.getNodeDisplayFile().write(writer);
+      expect(await writer.toStringFormatted()).toMatchSnapshot();
+    });
+  });
+});

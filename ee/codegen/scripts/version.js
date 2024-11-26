e8ec5af60a6875ece51d275fa9bc9e1d7ad3eae0
Noa Flaherty <noa@vellum.ai>
2024-11-25 23:54:10 -0500

    Fix Codegen Version Script & Increment (#189)
    * Fix script
    
    * Increment codegen

--- ee/codegen/scripts/version.js
+++ ee/codegen/scripts/version.js
@@ -7,6 +7,8 @@ const packageJsonPath = path.join(__dirname, "../package.json");
 const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
 const version = packageJson.version;
 
+const postVersionRegex = /^(\d+)\.(\d+)\.(\d+)(?:-post(\d+))?$/;
+
 if (typeof version !== "string") {
   throw new Error("Package version is not a string");
 }
@@ -15,8 +17,10 @@ if (version.match(/^\d+\.\d+\.\d+$/)) {
   const newVersion = `${version}-post1`;
   console.log(`Bumping version to ${newVersion}`);
   execSync(`npm version ${newVersion}`, { stdio: "inherit" });
-} else if (version.match(/^\d+\.\d+\.\d+-post\d+$/)) {
-  const [major, minor, patch, post] = version.split(".");
+} else if (version.match(postVersionRegex)) {
+  const match = version.match(postVersionRegex);
+  const [_, major, minor, patch, post] = match;
+
   const postNumber = Number(post);
   if (isNaN(postNumber)) {
     throw new Error(`Invalid post version format: ${post}`);

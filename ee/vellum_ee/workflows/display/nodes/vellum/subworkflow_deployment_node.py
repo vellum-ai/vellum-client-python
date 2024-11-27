8c8a3fcded7ddf00dfd98b7013399d1a67e1cf05
Pei Li <peihongli94@gmail.com>
2024-11-26 19:26:29 -0500

    add subworkflow deployment display overrides (#204)

--- ee/vellum_ee/workflows/display/nodes/vellum/subworkflow_deployment_node.py
+++ ee/vellum_ee/workflows/display/nodes/vellum/subworkflow_deployment_node.py
@@ -1,13 +1,13 @@
 from uuid import UUID
-from typing import Any, Generic, Optional, TypeVar
+from typing import Any, ClassVar, Dict, Generic, Optional, TypeVar
 
+from vellum.workflows.nodes import SubworkflowDeploymentNode
+from vellum.workflows.types.core import JsonObject
+from vellum.workflows.vellum_client import create_vellum_client
 from vellum_ee.workflows.display.nodes.base_node_vellum_display import BaseNodeVellumDisplay
 from vellum_ee.workflows.display.nodes.utils import raise_if_descriptor
 from vellum_ee.workflows.display.nodes.vellum.utils import create_node_input
 from vellum_ee.workflows.display.types import WorkflowDisplayContext
-from vellum.workflows.nodes import SubworkflowDeploymentNode
-from vellum.workflows.types.core import JsonObject
-from vellum.workflows.vellum_client import create_vellum_client
 
 _SubworkflowDeploymentNodeType = TypeVar("_SubworkflowDeploymentNodeType", bound=SubworkflowDeploymentNode)
 
@@ -15,6 +15,8 @@ _SubworkflowDeploymentNodeType = TypeVar("_SubworkflowDeploymentNodeType", bound
 class BaseSubworkflowDeploymentNodeDisplay(
     BaseNodeVellumDisplay[_SubworkflowDeploymentNodeType], Generic[_SubworkflowDeploymentNodeType]
 ):
+    subworkflow_input_ids_by_name: ClassVar[Dict[str, UUID]] = {}
+    
     def serialize(
         self, display_context: WorkflowDisplayContext, error_output_id: Optional[UUID] = None, **kwargs: Any
     ) -> JsonObject:
@@ -28,7 +30,7 @@ class BaseSubworkflowDeploymentNodeDisplay(
                 input_name=variable_name,
                 value=variable_value,
                 display_context=display_context,
-                input_id=None,
+                input_id=self.subworkflow_input_ids_by_name.get(variable_name),
             )
             for variable_name, variable_value in subworkflow_inputs.items()
         ]

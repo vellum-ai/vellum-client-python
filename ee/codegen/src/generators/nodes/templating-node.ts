f0518ab756b4eca5bc6248be31ba43244973e419
Harrison Ngo <harrison@vellum.ai>
2024-11-27 16:32:33 -0500

    [SC-4649] Remove templating node exclusion

--- ee/codegen/src/generators/nodes/templating-node.ts
+++ ee/codegen/src/generators/nodes/templating-node.ts
@@ -86,7 +86,13 @@ export class TemplatingNode extends BaseSingleFileNode<
         initializer: python.TypeInstantiation.uuid(
           this.nodeData.data.targetHandleId
         ),
-      })
+      }),
+      python.field({
+        name: "template_input_id",
+        initializer: python.TypeInstantiation.uuid(
+          this.nodeData.data.templateNodeInputId
+        ),
+      }),
     );
 
     return statements;
--- ee/codegen_integration/fixtures/simple_templating_node/code/display/nodes/templating_node.py
+++ ee/codegen_integration/fixtures/simple_templating_node/code/display/nodes/templating_node.py
@@ -12,6 +12,7 @@ class TemplatingNodeDisplay(BaseTemplatingNodeDisplay[TemplatingNode]):
     node_id = UUID("7dffcbb1-0a5c-4149-a6e9-f83095b0a871")
     output_id = UUID("4d39036a-fd6d-4a51-b410-7c0623375ebd")
     target_handle_id = UUID("3522e32d-6735-499b-8d45-c0c6488ae92f")
+    template_input_id = UUID("5d2f0f48-4504-4979-8e24-92a8c08c23a4")
     node_input_ids_by_name = {"template": UUID("5d2f0f48-4504-4979-8e24-92a8c08c23a4")}
     port_displays = {
         TemplatingNode.Ports.default: PortDisplayOverrides(id=UUID("9bce9d0a-3b0e-478e-8a97-f0510715a5a0"))

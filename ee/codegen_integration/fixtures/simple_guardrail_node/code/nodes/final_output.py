e371ecc68f7f323111959f4f7151cc537a95c956
Pei Li <peihongli94@gmail.com>
2024-11-28 14:45:39 -0500

    remove guardrail node exclusion (#243)
    * remove guardrail node exclusion
    
    * use score as output
    
    * add exclusion on codegen

--- ee/codegen_integration/fixtures/simple_guardrail_node/code/nodes/final_output.py
+++ ee/codegen_integration/fixtures/simple_guardrail_node/code/nodes/final_output.py
@@ -1,7 +1,9 @@
 from vellum.workflows.nodes.displayable import FinalOutputNode
 from vellum.workflows.state import BaseState
 
+from .guardrail_node import GuardrailNode
+
 
 class FinalOutput(FinalOutputNode[BaseState, float]):
     class Outputs(FinalOutputNode.Outputs):
-        value = None
+        value = GuardrailNode.Outputs.score

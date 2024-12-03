225d69db9a37f7821a0fba53c2697d7bfa480579
David Vargas <dvargas92495@gmail.com>
2024-12-03 15:29:34 -0500

    trickier await any example with conditional loops

---
+++ tests/workflows/await_any_conditional_loops/tests/test_workflow.py
@@ -0,0 +1,18 @@
+from tests.workflows.await_any_conditional_loops.workflow import AwaitAnyWithConditionalLoopsWorkflow
+from tests.workflows.basic_await_any.workflow import BasicAwaitAnyWorkflow
+
+
+def test_workflow__happy_path():
+    """
+    This test ensures that the Workflow completes successfully with a trickier AWAIT_ANY example.
+    """
+
+    # GIVEN a Workflow with a trickier AWAIT_ANY example
+    workflow = AwaitAnyWithConditionalLoopsWorkflow()
+
+    # WHEN the Workflow is run
+    final_event = workflow.run()
+
+    # THEN the Workflow completes successfully
+    assert final_event.name == "workflow.execution.fulfilled"
+    assert final_event.outputs.final_value == 1
---
+++ tests/workflows/await_any_conditional_loops/workflow.py
@@ -0,0 +1,88 @@
+import time
+
+from vellum.workflows.nodes.bases.base import BaseNode
+from vellum.workflows.ports.port import Port
+from vellum.workflows.state.base import BaseState
+from vellum.workflows.types.core import MergeBehavior
+from vellum.workflows.workflows.base import BaseWorkflow
+
+
+class StartNode(BaseNode):
+    pass
+
+
+class TopNode(BaseNode):
+    class Ports(BaseNode.Ports):
+        success = Port.on_if(StartNode.Execution.count.equals(1))
+        failure = Port.on_else()
+
+
+class TopDeadEndNode(BaseNode):
+    pass
+
+
+class BottomNode(BaseNode):
+    class Ports(BaseNode.Ports):
+        success = Port.on_if(StartNode.Execution.count.equals(2))
+        failure = Port.on_else()
+
+
+class BottomDeadEndNode(BaseNode):
+    pass
+
+
+class MergeNode(BaseNode):
+    class Trigger(BaseNode.Trigger):
+        merge_behavior = MergeBehavior.AWAIT_ANY
+
+
+class LoopNode(BaseNode):
+    class Ports(BaseNode.Ports):
+        loop = Port.on_if(MergeNode.Execution.count.equals(1))
+        exit = Port.on_else()
+
+
+class ExitNode(BaseNode):
+    class Outputs(BaseNode.Outputs):
+        final_value = LoopNode.Execution.count
+
+
+class AwaitAnyWithConditionalLoopsWorkflow(BaseWorkflow):
+    r"""
+    This is a trickier example of the AWAIT_ANY merge behavior.
+
+    Here's the Ascii diagram:
+
+                     TopDeadEndNode
+                     /
+              TopNode                       StartNode
+             /       \                     /
+    StartNode         MergeNode >> LoopNode
+            \       /                      \ 
+            BottomNode                      ExitNode
+                     \ 
+                    BottomDeadEndNode
+    
+    On the first iteration, the TopNode will complete and invoke the MergeNode, the BottomNode will deadend.
+    On the second iteration, the BottomNode will complete and invoke the MergeNode, the TopNode will deadend.
+
+    In the perspective of the MergeNode, it will complete once either the TopNode or the BottomNode complete. It
+    has no way of knowing whether the invocation of BottomNode was during the first or second iteration.
+    """
+
+    graph = StartNode >> {
+        TopNode.Ports.failure >> TopDeadEndNode,
+        {
+            TopNode.Ports.success,
+            BottomNode.Ports.success,
+        }
+        >> MergeNode
+        >> {
+            LoopNode.Ports.loop >> StartNode,
+            LoopNode.Ports.exit >> ExitNode,
+        },
+        BottomNode.Ports.failure >> BottomDeadEndNode,
+    }
+
+    class Outputs(BaseWorkflow.Outputs):
+        final_value = ExitNode.Outputs.final_value

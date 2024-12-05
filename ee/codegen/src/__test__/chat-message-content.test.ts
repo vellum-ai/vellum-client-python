c287dc81d720e9b33f354966b13da7e030d6c039
Kira <kawoods826@gmail.com>
2024-12-05 13:45:38 -0700

    Add support for ChatMessage image content type (#305)
    * Add support for ChatMessage audio content type
    
    * WIP: Write test
    
    * Revert "WIP: Write test"
    
    This reverts commit c7152eb945ea7d1f827bf43b547fe13aea70ddb1.
    
    * Revert "Add support for ChatMessage audio content type"
    
    This reverts commit a749c3bea967656681f911fc40797bfd5f91803b.
    
    * Add support for Images in ChatMessageRequest
    
    * Update to use inheritReference

--- ee/codegen/src/__test__/chat-message-content.test.ts
+++ ee/codegen/src/__test__/chat-message-content.test.ts
@@ -55,4 +55,21 @@ describe("ChatMessageContent", () => {
       expect(await writer.toStringFormatted()).toMatchSnapshot();
     });
   });
+
+  describe("IMAGE", () => {
+    it("should write an image content correctly", async () => {
+      const chatMessageContent = new ChatMessageContent({
+        chatMessageContent: {
+          type: "IMAGE",
+          value: {
+            src: "https://example.com/image.png",
+            metadata: { key: "value" },
+          },
+        },
+      });
+      chatMessageContent.write(writer);
+      expect(await writer.toStringFormatted()).toMatchSnapshot();
+      expect(chatMessageContent.getReferences()).toHaveLength(2);
+    });
+  });
 });

b1dd11f3b7669cd64094ae2c1c13ae1632d8faa6
David Vargas <vargas@vellum.ai>
2024-12-06 12:59:50 -0500

    Introduce function compilation (#330)

---
+++ src/vellum/workflows/utils/tests/test_functions.py
@@ -0,0 +1,16 @@
+from vellum.client.types.function_definition import FunctionDefinition
+from vellum.workflows.utils.functions import compile_function_definition
+
+
+def test_compile_function_definition__just_name():
+    # GIVEN a function with just a name
+    def my_function():
+        pass
+
+    # WHEN compiling the function
+    compiled_function = compile_function_definition(my_function)
+
+    # THEN it should return the compiled function definition
+    assert compiled_function == FunctionDefinition(
+        name="my_function",
+    )
